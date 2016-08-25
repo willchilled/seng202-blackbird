6d29ca41a7e7da5a373b950ac794701d05672cd9
Megan Chu <mch230@uclive.ac.nz>
2016-08-25 18:09:57 +1200

    Initial methods of filter.java

--- src/main/java/seng202/group2/blackbirdControl/Filter.java
+++ src/main/java/seng202/group2/blackbirdControl/Filter.java
@@ -1,8 +1,39 @@
 package seng202.group2.blackbirdControl;
 
+import seng202.group2.blackbirdModel.AirlinePoint;
+import seng202.group2.blackbirdModel.AirportPoint;
+import seng202.group2.blackbirdModel.RoutePoint;
+
+import java.util.ArrayList;
+
 /**
  * Created by mch230 on 25/08/16.
  */
 public class Filter {
+    public static ArrayList<AirportPoint> filterAirportCountry(ArrayList<AirportPoint> airports, String country) {
+        ArrayList<AirportPoint> filteredAirports = new ArrayList<AirportPoint>();
+        for (AirportPoint airport : airports) {
+            if (airport.getAirportCountry().equals(country)) {
+                filteredAirports.add(airport);
+            }
+        }
+        return filteredAirports;
+    }
 
+    public static ArrayList<AirlinePoint> filterAirlineCountry(ArrayList<AirlinePoint> airlines, String country) {
+        ArrayList<AirlinePoint> filteredAirlines = new ArrayList<AirlinePoint>();
+        for (AirlinePoint airline : airlines) {
+            if (airline.getCountry().equals(country)) {
+                filteredAirlines.add(airline);
+            }
+        }
+        return filteredAirlines;
+    }
+
+    public static void filterRoutes(ArrayList<RoutePoint> routes, String srcCountry, String destCountry) {
+        for (RoutePoint route : routes) {
+            //if (route.getSrcAirportID() && route.getDstAirportID()) {   //should airport be inside the route point? - linking of data
+
+        }
+    }
 }

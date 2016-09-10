d10c0483378813eb61f642ea99829768ae35c7bf
LizRichardson <emr65@uclive.ac.nz>
2016-09-10 18:48:36 +1200

    Have reduced the data showing in the aiport and airline tables and added popups with more info when table entry is double clicked

---
+++ src/main/java/seng202/group2/blackbirdView/AirlinePopUpController.java
@@ -0,0 +1,51 @@
+package seng202.group2.blackbirdView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import seng202.group2.blackbirdModel.AirlinePoint;
+
+public class AirlinePopUpController {
+
+    private AirlinePoint airlinePoint;
+
+/**
+    public AirlinePopUpController(AirlinePoint airlinePoint) {
+        this.airlinePoint = airlinePoint;
+        this.airlineName = airlinePoint.getAirlineName();
+    }**/
+
+    @FXML private Label nameText;
+    @FXML private Label idText;
+    @FXML private Label countryText;
+    @FXML private Label aliasText;
+    @FXML private Label iataText;
+    @FXML private Label icaoText;
+    @FXML private Label callsignText;
+    @FXML private Label activeText;
+
+
+    @FXML
+    void setUpPopUp(){
+        System.out.println("here!");
+        nameText.setText(airlinePoint.getAirlineName());
+        idText.setText(String.valueOf(airlinePoint.getAirlineID()));
+        countryText.setText(airlinePoint.getCountry());
+        aliasText.setText(airlinePoint.getAirlineAlias());
+        iataText.setText(airlinePoint.getIata());
+        icaoText.setText(airlinePoint.getIcao());
+        callsignText.setText(airlinePoint.getCallsign());
+        activeText.setText(airlinePoint.getActive());
+
+
+      //  nameText.setVisible(true);
+        //nameText.setText("Hi");
+        //String ms = nameText.getText();
+    }
+
+    public void setAirlinePoint(AirlinePoint airlinePoint) {
+        this.airlinePoint = airlinePoint;
+    }
+}
+

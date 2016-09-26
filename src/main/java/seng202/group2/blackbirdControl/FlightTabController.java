bf4fe8866afacfc03fffc6e3db1312040ebee609
William Muir <wmu16@uclive.ac.nz>
2016-09-26 15:36:25 +1300

    Added map functionality for flights

--- src/main/java/seng202/group2/blackbirdControl/FlightTabController.java
+++ src/main/java/seng202/group2/blackbirdControl/FlightTabController.java
@@ -11,6 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import seng202.group2.blackbirdModel.*;
 
@@ -28,6 +30,11 @@ public class FlightTabController {
     private MainController mainController;
 
     private Flight flight;
+    private FlightPoint flightPoint;
+
+    //MapShit
+    @FXML private WebView webView;
+    private WebEngine webEngine;
 
     //FLIGHT and FLIGHT POINT tables
     @FXML private TableView<DataPoint> flightTable;
@@ -65,6 +72,7 @@ public class FlightTabController {
 
         flightDstICAOMenu.setValue(flightDstICAOList.get(0));
         flightDstICAOMenu.setItems(flightDstICAOList);
+        initMap();
     }
 
 
@@ -212,6 +220,9 @@ public class FlightTabController {
 
                     Flight pressedFlight = (Flight) flightTable.getSelectionModel().getSelectedItem();
                     flight = pressedFlight;
+                    Route myRoute = new Route(Route.makeRoutePoints(flight));
+                    displayRoute(myRoute);
+
 
                     flightPointTable.getItems().setAll(pressedFlight.getFlightPoints());
 
@@ -222,6 +233,24 @@ public class FlightTabController {
                     flightPointLongitudeCol.setCellValueFactory(new PropertyValueFactory<DataPoint, String>("longitude"));
                 }
 
+            }
+        });
+
+        flightPointTable.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+
+                    FlightPoint pressedPoint = (FlightPoint) flightPointTable.getSelectionModel().getSelectedItem();
+                    flightPoint = pressedPoint;
+                    double lat = flightPoint.getLatitude();
+                    double lng = flightPoint.getLongitude();
+                    ArrayList<Position> myWayPoint = new ArrayList<>();
+                    myWayPoint.add(new Position(lat, lng));
+                    Route myMarker = new Route(myWayPoint);
+                    String scriptToExecute = "displayWayPoint(" + myMarker.toJSONArray() + ")";
+                    webEngine.executeScript(scriptToExecute);
+                }
             }
         });
 
@@ -255,5 +284,24 @@ public class FlightTabController {
             DataBaseRefactor.editDataEntry(sql);
             flightFilterButtonPressed();
         }
+    }
+
+    //______________________MAP STUFf--------------------------------------//
+
+    /**
+     * Initializes the map with the JavaScript
+     */
+    private void initMap() {
+        webEngine = webView.getEngine();
+        webEngine.load(getClass().getClassLoader().getResource("map.html").toExternalForm());
+    }
+
+    /**
+     *Displays a route on the map by way of making a javascript and executing it through the web engine
+     * @param newRoute
+     */
+    private void displayRoute(Route newRoute) {
+        String scriptToExecute = "displayRoute(" + newRoute.toJSONArray() + ");";
+        webEngine.executeScript(scriptToExecute);
     }
 }

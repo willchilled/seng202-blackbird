0879eae9ddee561c4149541bf8bb7c2b00445740
Megan Chu <mch230@uclive.ac.nz>
2016-09-27 20:38:06 +1300

    Adding alert messages back in

--- src/main/java/seng202/group2/blackbirdModel/ParserRefactor.java
+++ src/main/java/seng202/group2/blackbirdModel/ParserRefactor.java
@@ -70,7 +70,7 @@ public class ParserRefactor {
             if ("\\N".equals(line)){
                 line = "";
             }
-            if (line.matches("([a-zA-Z0-9]+\\\\'[a-zA-Z0-9]+)+")){
+            if (line.matches("([a-zA-Z0-9]+\\\\\\\\'[a-zA-Z0-9]+)+")){
                 line = line.replaceAll("\\\\", "");
             }
             formattedLine[lineCount] = line;

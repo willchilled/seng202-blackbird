a1149614bbc8f11f23a562308c70e1f5b214d15a
William Muir <wmu16@uclive.ac.nz>
2016-09-30 21:31:48 +1300

    Fixing table formatting for error tab

--- src/main/java/seng202/group2/blackbirdView/Main.java
+++ src/main/java/seng202/group2/blackbirdView/Main.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icon.png")));
         primaryStage.setTitle("Blackbird");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);

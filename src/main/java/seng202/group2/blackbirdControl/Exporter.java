4b1e056a2a208d1bae6b656119cd2030fa781d83
Megan Chu <mch230@uclive.ac.nz>
2016-10-02 16:44:09 +1300

    Fixed cancel export error

--- src/main/java/seng202/group2/blackbirdControl/Exporter.java
+++ src/main/java/seng202/group2/blackbirdControl/Exporter.java
@@ -4,6 +4,7 @@ package seng202.group2.blackbirdControl;
  * Created by emr65 on 15/09/16.
  */
 
+import javafx.scene.control.Alert;
 import seng202.group2.blackbirdModel.AirportPoint;
 import seng202.group2.blackbirdModel.DataBaseRefactor;
 import seng202.group2.blackbirdModel.DataPoint;
@@ -19,26 +20,39 @@ public class Exporter {
 
     public static void exportData(ArrayList<DataPoint> points){
 
+
         File fileToSave = HelperFunctions.getFile("Save File", true);
+        if (fileToSave == null) {
+            return;
+        }
 
-            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream(fileToSave), "utf-8"))) {
-                
-                for(int i = 0; i < points.size(); i++){
-                    writer.write(points.get(i).toString()+"\n");
-                 //   System.out.println(points.get(i).toString());
-                }
+        System.out.println(points.size());
+        if (points.size() == 0) {   //In the case of other data types, the export button is disabled if empty, so error message is specific to flights
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No flight selected for export");
+            alert.setContentText("Please select a flight to be exported first");
+            alert.showAndWait();
+            return;
+        }
 
-                    //writer.write("something");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileToSave), "utf-8"))) {
+
+            for(int i = 0; i < points.size(); i++){
+                writer.write(points.get(i).toString()+"\n");
+             //   System.out.println(points.get(i).toString());
             }
 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+    }
 
     /**
      * A method to save the database under a filename
